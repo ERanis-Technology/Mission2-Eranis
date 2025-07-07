23fa3b2a87647f7c4a75010c4e19cfe81b8c3e5e
ONAMBELE KONI <bryan.koni@facsciences-uy1.cm>
2025-07-07 09:08:14 +0100

    Initialisation du CRUD sur les missions

--- Backend/professionnels/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import createProfessionnel
-
-app_name = 'professionnel'
-
-urlpatterns = [
-    path('create_pro', createProfessionnel,name='create_pro'),
-]
